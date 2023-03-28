d4e878b91896b42b9686f69b81b933409105e9c6
juanma01rs <juanma10rs@gmail.com>
2023-03-27 20:20:43 -0500

    10.1

--- src/main/java/vehiculos/Vehiculo.java
+++ src/main/java/vehiculos/Vehiculo.java
@@ -28,7 +28,7 @@ public class Vehiculo {
 	}
 	
 	public static String vehiculosPorTipo() {
-		return ("Automoviles: " + Automovil.getCantidadAutomoviles + "\nCamionetas: " + Camion.getCantidadCamiones + "\nCamiones: " + Camioneta.getCantidadCamionetas);
+		return "Automoviles: " + Automovil.getCantidadAutomoviles + "\nCamionetas: " + Camioneta.getCantidadCamionetas + "\nCamiones: " + Camion.getCantidadCamiones;
 	}
 	
 	public void setPlaca(String placa) {
